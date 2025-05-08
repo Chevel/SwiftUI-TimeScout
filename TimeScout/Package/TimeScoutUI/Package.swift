305b0427d7628544ebdb8a68afade435c29391a7
Matej Kokosinek <matej.dev@blauberg.tech>
2025-05-08 16:14:02 +0200

    feat: Define core dependency explicitly

--- TimeScout/Package/TimeScoutUI/Package.swift
+++ TimeScout/Package/TimeScoutUI/Package.swift
@@ -12,11 +12,16 @@ let package = Package(
             name: "TimeScoutUI",
             targets: ["TimeScoutUI"]),
     ],
+    dependencies: [
+        .package(path: "TimeScoutCore")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TimeScoutUI"),
+            name: "TimeScoutUI",
+            dependencies: [.product(name: "TimeScoutCore", package: "TimeScoutCore")]
+        ),
         .testTarget(
             name: "TimeScoutUITests",
             dependencies: ["TimeScoutUI"]
