34c9d172af1041e7b8366d85148e0630d1c6e59a
Matej Kokosinek <matej.dev@blauberg.tech>
2025-05-30 15:59:08 +0200

    chore: Separate AppSettings and project settings

--- TimeScout/Package/TimeScoutCore/Sources/TimeScoutCore/Configuration/AppSettings.swift
+++ TimeScout/Package/TimeScoutCore/Sources/TimeScoutCore/Configuration/AppSettings.swift
@@ -9,43 +9,8 @@
 import Foundation
 
 public enum AppSettings {
-    
-    static var environment: Environment {
-        #if DEV
-        return .dev
-        #else
-        return .prod
-        #endif
-    }
-
-    public enum Environment {
-        case dev, prod
-        
-        public static var isDev: Bool {
-            return environment == .dev
-        }
-    }
-    
-    public static var target: Version {
-        #if PRO
-        return .pro
-        #else
-        return .basic
-        #endif
-    }
-
-    public enum Version {
-        case basic, pro
-    }
 
     public enum Constants {
-        
-        public static var appStoreURL: URL {
-            switch AppSettings.target {
-            case .basic: return URL(string: "https://apps.apple.com/us/app/timescout/id1584949806")!
-            case .pro: return URL(string: "https://apps.apple.com/us/app/timescoutpro/id1584951815")!
-            }
-        }
 
         public static var alphabetCharacters: [String] {
             guard let alphabetCharacterSet = Locale.current.exemplarCharacterSet?.intersection(CharacterSet.lowercaseLetters) else {
