6c6d65487a7988a526b538b8572bd6d088b8c753
Matej Kokosinek <matej.dev@blauberg.tech>
2025-09-08 16:24:14 +0200

    feat: Migrate Upsell button, add Core subdependency to UI

--- TimeScout/Package/TimeScoutUI/Package.swift
+++ TimeScout/Package/TimeScoutUI/Package.swift
@@ -12,13 +12,21 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TimeScoutUI",
-            targets: ["TimeScoutUI"]),
+            targets: ["TimeScoutUI"]
+        ),
+    ],
+    dependencies: [
+        .package(name: "TimeScoutCore", path: "TimeScoutCore")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TimeScoutUI"),
+            name: "TimeScoutUI",
+            dependencies: [
+                "TimeScoutCore"
+            ]
+        ),
         .testTarget(
             name: "TimeScoutUITests",
             dependencies: ["TimeScoutUI"]
